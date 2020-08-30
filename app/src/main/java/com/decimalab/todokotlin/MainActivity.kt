bcae685d38d41751bdc4db63e9a203679de72172
SHAZ <shakilahmedshaj@gmail.com>
2020-08-30 15:01:13 +0600

    Initial commit

---
+++ app/src/main/java/com/decimalab/todokotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.decimalab.todokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
