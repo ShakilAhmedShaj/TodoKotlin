e414ff79bf47e08b585c52d13795b3f9c3b474ee
SHAZ <shakilahmedshaj@gmail.com>
2020-08-31 20:42:25 +0600

    sorting added

--- app/src/main/java/com/decimalab/todokotlin/data/viewmodel/ToDoViewModel.kt
+++ app/src/main/java/com/decimalab/todokotlin/data/viewmodel/ToDoViewModel.kt
@@ -21,10 +21,14 @@ class ToDoViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: ToDoRepository
 
     val getAllData: LiveData<List<ToDoData>>
+    val sortByHighPriority: LiveData<List<ToDoData>>
+    val sortByLowPriority: LiveData<List<ToDoData>>
 
     init {
         repository = ToDoRepository(toDoDao)
         getAllData = repository.getAllData
+        sortByHighPriority = repository.sortByHighPriority
+        sortByLowPriority = repository.sortByLowPriority
 
     }
 
--- app/src/main/java/com/decimalab/todokotlin/fragments/list/ListFragment.kt
+++ app/src/main/java/com/decimalab/todokotlin/fragments/list/ListFragment.kt
@@ -112,6 +112,8 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_delete_all -> confirmRemoval()
+            R.id.menu_priority_high -> mToDoViewModel.sortByHighPriority.observe(this, Observer { adapter.setData(it) })
+            R.id.menu_priority_low -> mToDoViewModel.sortByLowPriority.observe(this, Observer { adapter.setData(it) })
         }
         return super.onOptionsItemSelected(item)
     }
